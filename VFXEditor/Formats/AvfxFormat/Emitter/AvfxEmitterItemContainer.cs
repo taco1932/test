effcd976fbf5e7c588c23a0254f50c360a97031b
EchoDidAThing <154573455+EchoDidAThing@users.noreply.github.com>
2025-04-26 22:14:11 -0400

    Handling for ITPR sizes 300 & 276

--- VFXEditor/Formats/AvfxFormat/Emitter/AvfxEmitterItemContainer.cs
+++ VFXEditor/Formats/AvfxFormat/Emitter/AvfxEmitterItemContainer.cs
@@ -1,5 +1,8 @@
+using Lumina.Excel.Sheets;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace VfxEditor.AvfxFormat {
     public class AvfxEmitterItemContainer : AvfxBase {
@@ -14,7 +17,24 @@ namespace VfxEditor.AvfxFormat {
         }
 
         public override void ReadContents( BinaryReader reader, int size ) {
-            for( var i = 0; i < size / 312; i++ ) Items.Add( new AvfxEmitterItem( IsParticle, Emitter, false, reader ) );
+            //for( var i = 0; i < size / 312; i++ ) Items.Add( new AvfxEmitterItem( IsParticle, Emitter, false, reader ) );
+            if(  ( float )size / 312  == size / 312  )
+            {
+                for( var i = 0; i < size / 312; i++ ) Items.Add( new AvfxEmitterItem( IsParticle, Emitter, false, 312, reader ) );
+            }
+            else if( (float)size / 300 == size / 300  )
+            {
+                for( var i = 0; i < size / 300; i++ ) Items.Add( new AvfxEmitterItem( IsParticle, Emitter, false, 300, reader ) );
+            }
+            else if( ( float )size / 276 == size / 276 )
+            {
+                for( var i = 0; i < size / 276; i++ ) Items.Add( new AvfxEmitterItem( IsParticle, Emitter, false, 276, reader ) );
+            }
+            else
+            {
+                Dalamud.Log( "size " + size.ToString() + " cannot be parsed");
+            }
+
         }
 
         protected override IEnumerable<AvfxBase> GetChildren() {
