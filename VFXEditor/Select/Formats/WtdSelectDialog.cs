ebad37a630490441f83b3d82fb1da1b48ab9f781
EchoDidAThing <154573455+EchoDidAThing@users.noreply.github.com>
2025-05-12 02:48:44 -0400

    I forgot to squish an atch.

--- VFXEditor/Select/Formats/WtdSelectDialog.cs
+++ VFXEditor/Select/Formats/WtdSelectDialog.cs
@@ -3,7 +3,7 @@ using VfxEditor.Select.Tabs.Common;
 
 namespace VfxEditor.Select.Formats {
     public class WtdSelectDialog : SelectDialog {
-        public WtdSelectDialog( string id, WtdManager manager, bool isSourceDialog ) : base( id, "atch", manager, isSourceDialog ) {
+        public WtdSelectDialog( string id, WtdManager manager, bool isSourceDialog ) : base( id, "wtd", manager, isSourceDialog ) {
             GameTabs.AddRange( [
                 new CommonTabWtd( this, "Common" )
             ] );
