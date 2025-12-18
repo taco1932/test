03d8756f8a1b183a4ff9a0a6ede9ba476eba1d29
taco1932 <127606210+taco1932@users.noreply.github.com>
2025-12-18 01:20:19 -0800

    rename C173 for clarity

--- VFXEditor/Formats/TmbFormat/Entries/C173.cs
+++ VFXEditor/Formats/TmbFormat/Entries/C173.cs
@@ -22,7 +22,7 @@ namespace VfxEditor.TmbFormat.Entries {
 
     public class C173 : TmbEntry {
         public const string MAGIC = "C173";
-        public const string DISPLAY_NAME = "VFX";
+        public const string DISPLAY_NAME = "Async VFX";
         public override string DisplayName => DISPLAY_NAME;
         public override string Magic => MAGIC;
 
